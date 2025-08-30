a11f0410f3b08fb6e34af2a930b220a8aa0f3f34
sira <Siras-MacBook-Pro-2@local>
2025-08-30 22:58:36 +0700

    chore: add Pages workflow and Vite base for GH Pages

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // Needed for GitHub Pages under repo subpath
+  base: "/compass-web/",
+});
+
+
